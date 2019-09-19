216d7661d45d75282a2e82348847a0889e484971
makinoshi <masa.kinoshita02@gmail.com>
2019-09-19 19:56:45 +0900

    Add generating codes

--- dev/user.clj
+++ dev/user.clj
@@ -15,3 +15,24 @@
 
 (defn cljs-repl []
   (fig/cljs-repl "dev"))
+
+(comment
+ (require '[camel-snake-kebab.core :as csk]
+          '[clojure.string :as str])
+
+ (let [components (->> (slurp "https://raw.githubusercontent.com/Semantic-Org/Semantic-UI-React/v0.88.1/src/index.js")
+                       (str/split-lines)
+                       ;; Only named exports
+                       (filter #(re-matches #"export\s[A-z].+from.+$" %))
+                       (map #(re-find #"export\s([A-z]+)\sfrom" %))
+                       (map second))]
+   ;; Generate jsObj definitions
+   (->> components
+        (map (fn [s] (format "(def js%s (get-obj \"%s\"))" s s)))
+        (str/join "\n")
+        (spit "/tmp/jsobj.txt"))
+   ;; Generate Reagent components
+   (->> components
+        (map (fn [s] (format "(def %s (adapt js%s))" s s)))
+        (str/join "\n")
+        (spit "/tmp/adapt.txt"))))
